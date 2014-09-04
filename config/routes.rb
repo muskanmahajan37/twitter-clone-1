ecfaa3c6292c9cee278540569cb252f70db3180e
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-04 15:00:09 -0700

    Add root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, :controllers => { :registrations => "users/registrations" }
 end
