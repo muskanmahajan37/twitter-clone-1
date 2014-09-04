2ae87ec320e78ca942829b1450736271db41d3e5
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-04 15:11:52 -0700

    Add email confirmation for new user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :send_welcome_message
+
+  validates_presence_of :name
+
+  def send_welcome_message
+    UserMailer.signup_confirmation(self).deliver
+  end
 end
