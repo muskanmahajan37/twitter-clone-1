c183c7598e54a64a85c03dd745160fde4b3db60f
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-04 16:21:47 -0700

    Update registration controller path on routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "posts#index"
 
 
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, :controllers => { :registrations => "users/registrations" }
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, :controllers => { :registrations => "registrations" }
 
   resources :posts
 end
