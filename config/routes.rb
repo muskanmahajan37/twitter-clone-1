10106eccb1cbc73cce6ebabd1f8880c99e986ce6
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-04 15:20:43 -0700

    Add Post routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     root to: "devise/sessions#new"
   end
+
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, :controllers => { :registrations => "users/registrations" }
+
+  resources :posts
 end
