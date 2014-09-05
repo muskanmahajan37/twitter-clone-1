aec32ec32cafe85e7bb0e66b58be284a4f33f8f4
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-05 10:32:47 -0700

    Add tests for user mention email

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -21,4 +21,23 @@ RSpec.describe UserMailer, :type => :mailer do
     end
   end
 
+  describe "mention" do
+    before(:each) do
+      ActionMailer::Base.deliveries.clear
+      @user = FactoryGirl.create(:user)
+      @other_user = FactoryGirl.create(:user)
+    end
+
+    let(:mail) { UserMailer.mention(@user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("You were Twisted bro")
+      expect(mail.to).to eq([@user.email])
+      expect(mail.from).to eq(["get@twisted.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to_not be_empty
+    end
+  end
 end
